1e484bd213e85065ce7f383221756167160cda2a
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-11-26 08:35:32 -0300

    feat[PDF]: separando PDF por página dinamicamemte

--- Automação/pdf_operations.py
+++ Automação/pdf_operations.py
@@ -30,10 +30,22 @@ def split_pdf(pdf_path):
             with open(output_filename, "wb") as out:
                 writer.write(out)
             print(f"PDF criado com o nome: {output_filename}")
+            
+def get_pdf_upto(pdf_path, start_page:int=0,stop_page:int=0):
+    with open(pdf_path, "rb") as f:
+        reader = PdfReader(f)
+        writer = PdfWriter()
+        for page_num in range(start_page, stop_page):
+            selected_page = reader.pages[page_num]
+            writer.add_page(selected_page)
+            filename = os.path.split(pdf_path)[1]
+            output_filename = f"files/{filename}_from_{start_page+1}_to_{stop_page+1}.pdf"
+        with open(output_filename, "wb") as out:
+            writer.write(out)
 
 # print(get_pdf_metadata("files/sample.pdf"))
 # print(get_pdf_metadata("files/sample.pdf").title)
 # print(get_pdf_metadata("files/sample.pdf").author)
 # print(extract_text_from_pdf("files/sample.pdf"))
-split_pdf("files/teste.pdf")
-
+# bvsplit_pdf("files/teste.pdf")
+get_pdf_upto("files/teste.pdf", 1, 2)
