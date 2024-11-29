485f4d09f6734569408e62242850f8b0143e9a1b
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-11-29 18:08:31 -0300

    feat[WebScrapping]: Criando acess_token

--- Web-Scraping/6-paginando_results.py
+++ Web-Scraping/6-paginando_results.py
@@ -3,7 +3,7 @@ from collections import Counter
 import pandas as pd
 
 # 1 - Utilizando Access Token
-access_token = '' # token gerado pelo github
+# access_token = '' # token gerado pelo github
 headers = {'Authorization': 'Bearer ' +access_token,
            'X-GitHub-Api-Version': '2022-11-28'
            }
