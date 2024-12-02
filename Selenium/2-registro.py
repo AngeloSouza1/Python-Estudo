b4bedd0f9dcffdad4a6bc50141e0312a1095d0cb
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-12-02 11:56:05 -0300

    feat[SELENIUM]: Unindo os conceitos II

--- Selenium/2-registro.py
+++ Selenium/2-registro.py
@@ -31,8 +31,8 @@ try:
 
     # 3 - Buscando informações
     results = browser.find_elements(By.TAG_NAME, 'strong')
-    # import pdb
-    # pdb.set_trace()
+    import pdb
+    pdb.set_trace()
     print(f'Domínio {results[1].text} está {results[2].text}')
 
 
