e2e8cde26e0a7edce60cf2aa350f043794918809
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-12-02 11:41:30 -0300

    feat[SELENIUM]: Unindo os conceitos I

--- Selenium/2-registro.py
+++ Selenium/2-registro.py
@@ -1,17 +1,33 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 import time
 
-# 1 - Acessando o site
+# Configuração do GeckoDriver
+firefox_service = Service('/usr/local/bin/geckodriver')  # Caminho do GeckoDriver
+firefox_options = Options()
+# firefox_options.add_argument('--headless')  # Executa em modo headless (opcional)
 
-browser = webdriver.Firefox()
-browser.get('<https://registro.br>')
+# Iniciando o navegador com as configurações
+browser = webdriver.Firefox(service=firefox_service, options=firefox_options)
 
-# 2 - Buscando elementos
-elem = browser.find_element(By.ID, 'is-avail-field')
-elem.clear()
-elem.send_keys('botscompython.com.br')
-elem.send_keys(Keys.ENTER)
-time.sleep(5)
-browser.save_screenshot('dominio.png')
+try:
+    # 1 - Acessando o site
+    browser.get('https://registro.br')
+
+    # 2 - Buscando elementos
+    elem = browser.find_element(By.ID, 'is-avail-field')
+    elem.clear()
+    elem.send_keys('botscompython.com.br')
+    elem.send_keys(Keys.ENTER)
+    
+    time.sleep(5)  # Aguarda o carregamento dos resultados
+    
+    # Salvando captura de tela
+    browser.save_screenshot('files/dominio.png')
+
+finally:
+    # Encerra o navegador
+    browser.quit()
