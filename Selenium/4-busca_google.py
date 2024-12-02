7ff16f12c131e2f00cf6986c47915a171ab24eac
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-12-02 15:41:11 -0300

    feat[SELENIUM]: Navegando entre páginas

--- Selenium/4-busca_google.py
+++ Selenium/4-busca_google.py
@@ -42,6 +42,29 @@ try:
     # Melhorando a formatação do print
     print(f"Total de páginas estimadas: {tot_pages:,.0f}".replace(",", "."))
 
+    # 7 - Navegando entre páginas
+    url_page = browser.find_element(
+        By.XPATH,
+        '//a[@aria-label="Page 2"]').get_attribute('href')
+
+    current_page = 0
+    start = 10
+    list_results = []
+
+    while current_page <= 10:
+        if not current_page == 0:
+            url_page = url_page.replace(
+                "start=%s" %start,
+                "start=%s" %(start + 10),
+            )
+            start += 10
+        current_page += 1
+        browser.get(url_page)
+
+
+
+
+
 
 finally:
     # Garante o fechamento do navegador
