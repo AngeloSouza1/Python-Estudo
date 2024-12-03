e5f22f0c9cbf52c8d5ababd2545ac7366d6c484b
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-12-03 15:46:10 -0300

    feat[IA]:Operações com Imagens I

--- AplicacaoIA/5-analise_imagem.py
+++ AplicacaoIA/5-analise_imagem.py
@@ -2,8 +2,25 @@ from PIL import Image, ImageEnhance, ImageFilter
 
 # 1 - Importando a Imagem / Convertendo a imagem em escala de cinza
 img = Image.open('data/moto.png')
-print(img)
+# print(img)
 img.show()
 
 gray_img = img.convert('L')
-gray_img.show()
+# gray_img.show()
+
+# 2 - Utilizando Operações em Imagem I
+rotated_img = img.rotate(180)
+# rotated_img.show()
+
+transpose_img = img.transpose(Image.FLIP_LEFT_RIGHT)
+# transpose_img.show()
+
+resize_img_small = img.resize((300, 500))
+# resize_img_small.show()
+
+resize_img_big = img.resize((900, 1000))
+# resize_img_big.show()
+
+dim = (200, 200, 250, 300)
+crop_img = img.crop(dim)
+# crop_img.show()
