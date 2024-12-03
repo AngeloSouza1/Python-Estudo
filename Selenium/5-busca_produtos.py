832af5eb29293d40c05f5aa9d35926553af3c5c5
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-12-03 10:13:22 -0300

    feat[SELENIUM]:  Busca Amazon II

--- Selenium/5-busca_produtos.py
+++ Selenium/5-busca_produtos.py
@@ -16,6 +16,30 @@ time.sleep(2)
 # 3 - Encontrando os elementos de todos os resultados
 element = browser.find_element(By.CSS_SELECTOR,
     'div.s-main-slot.s-result-list.s-search-results.sg-row'
+    
 )
 print(element)
 time.sleep(2)
+
+# 4 - Encontrar informações dos produtos
+items = element.find_elements(
+    By.XPATH,
+    '//div[@data-component-type="s-search-result"]'
+)
+print(len(items))
+
+for item in items:
+    title = item.find_element(By.TAG_NAME, 'h2').text
+    price = ""
+
+    try:
+        price = item.find_element(
+            By.CLASS_NAME,
+            'a-price'
+        ).text.replace('\n', '.')
+    except:
+        pass    
+   
+    
+    print(f"Título: {title}")
+    print(f"Preço: {price}")
