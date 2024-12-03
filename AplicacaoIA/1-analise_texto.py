6017ad4f8ae006cf3b50d0560e54b5d7261cfe25
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-12-03 12:06:25 -0300

    feat[IA]: Frequência de Distribuição

--- AplicacaoIA/1-analise_texto.py
+++ AplicacaoIA/1-analise_texto.py
@@ -1,6 +1,8 @@
 import os
 import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.probability import FreqDist
+import matplotlib.pyplot as plt
 
 # Baixar o recurso 'punkt', se necessário
 nltk.download('punkt')
@@ -12,8 +14,8 @@ file_path = os.path.join("data", "texto.txt")
 if os.path.exists(file_path):  # Verificar se o arquivo existe
     with open(file_path, "r", encoding="utf-8") as file:
         texto = file.read()
-        print("Texto importado com sucesso:")
-        print(texto)
+        print("Texto importado com sucesso!")
+        # print(texto)
 else:
     print(f"Erro: O arquivo '{file_path}' não foi encontrado.")
     exit()  # Sair do programa se o arquivo não existir
@@ -33,3 +35,13 @@ try:
 except LookupError as e:
     print(f"Erro ao realizar a tokenização: {e}")
     print("Certifique-se de que os pacotes necessários do NLTK estão instalados.")
+
+# 3 - Frequência de Distribuição
+print("\n--- Frequência de Distribuição ---")
+fdist = FreqDist(word_tokens)
+print(fdist.most_common(10))
+
+# Plotando o gráfico ou salvando como imagem
+fdist.plot(10)
+plt.savefig("frequencia_distribuicao.png")
+print("Gráfico salvo como 'frequencia_distribuicao.png'")
