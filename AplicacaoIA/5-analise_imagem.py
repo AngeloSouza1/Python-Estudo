3a2ae651e377da8eafb7c5c712435b085d00d8d2
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-12-03 15:38:07 -0300

    feat[IA]: Importando uma imagem

--- AplicacaoIA/5-analise_imagem.py
+++ AplicacaoIA/5-analise_imagem.py
@@ -1,9 +1,9 @@
 from PIL import Image, ImageEnhance, ImageFilter
 
 # 1 - Importando a Imagem / Convertendo a imagem em escala de cinza
-img = Image.open('data/DKC2.jpg')
-# print(img)
+img = Image.open('data/moto.png')
+print(img)
 img.show()
 
 gray_img = img.convert('L')
-# gray_img.show()
+gray_img.show()
