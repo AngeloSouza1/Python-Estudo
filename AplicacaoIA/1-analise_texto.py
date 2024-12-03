8f03952db3edb128440bf6938fe1a1490e64a627
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-12-03 11:33:26 -0300

    feat[IA]: Tokenizando o Texto

--- AplicacaoIA/1-analise_texto.py
+++ AplicacaoIA/1-analise_texto.py
@@ -5,3 +5,11 @@ with open(os.path.join("data", "texto.txt"), "r", encoding="utf-8") as file:
     texto = file.read()
     print(texto)
     
+# 2 - Tokenizando o Texto
+sent_tokens = sent_tokenize(texto)
+print(sent_tokens)
+print(len(sent_tokens))
+
+word_tokens = word_tokenize(texto)
+print(word_tokens)
+print(len(word_tokens))
