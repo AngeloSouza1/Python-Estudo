a5dde6d041954218b2cda13e4e33f27c1b76c658
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-12-09 12:12:43 -0300

    feat: Recuperando Valores em Variáveis

--- Flask/app.py
+++ Flask/app.py
@@ -5,8 +5,16 @@ app = Flask(__name__)
 # localhost:5000/
 @app.route('/')
 def principal():
+    conteudo1 = 'Manipulação de Dados'
+    conteudo2 = 'Herança e Templates'
+    conteudo3 = 'Integração de APIs'
+    conteudo4 = 'Banco de Dados'
     return render_template(
-        "index.html"
+        "index.html",
+        conteudo1=conteudo1,
+        conteudo2=conteudo2,
+        conteudo3=conteudo3,
+        conteudo4=conteudo4
     )
 
 @app.route('/sobre')
