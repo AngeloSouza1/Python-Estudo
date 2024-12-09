ea3dba35e2d29cd8204f75f9f56a27e9248b4911
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-12-09 12:03:46 -0300

    feat: Testando Novas Rotas e Integração com HTML

--- Flask/app.py
+++ Flask/app.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # localhost:5000/
 @app.route('/')
 def principal():
-    return "<h2>Hello World</h2>"
+    return render_template(
+        "index.html"
+    )
+
+@app.route('/sobre')
+def sobre():
+    return render_template("sobre.html")
