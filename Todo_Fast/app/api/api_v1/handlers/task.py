568ed2f0f0b921db9a9a800e38cb1379e5b8c73e
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-12-27 14:01:39 -0300

    adiconando schema

--- Todo_Fast/app/api/api_v1/handlers/task.py
+++ Todo_Fast/app/api/api_v1/handlers/task.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from app.schemas.task_schema import TaskDetail
+from app.models.user_model import User
+from app.api.dependencies.user_deps import get_current_user
 
 task_router = APIRouter()
 
-@task_router.get('/test')
-async def teste():
-    return "Tarefas Listadas"
+@task_router.get('/', summary='Lista as Tarefas', response_model=TaskDetail)
+async def list_tasks(user: User = Depends(get_current_user)):
+    pass
