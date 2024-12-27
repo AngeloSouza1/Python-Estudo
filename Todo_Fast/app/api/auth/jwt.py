3d482b13f0a0d149718a28a1924dff48db5665da
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-12-27 10:19:25 -0300

    chore: testanto Login

--- Todo_Fast/app/api/auth/jwt.py
+++ Todo_Fast/app/api/auth/jwt.py
@@ -1,13 +1,25 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from typing import Any
-from services.user_service import UserService
+from app.services.user_service import UserService
+from app.core.security import create_access_token, create_refresh_token
 
 auth_router = APIRouter()
 
 @auth_router.post('/login')
 async def login(data: OAuth2PasswordRequestForm = Depends()) -> Any:
     usuario = await UserService.authenticate(
-        email = data.email,
+        email = data.username,
         password = data.password
     )
+    
+    if not usuario:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='E-mail ou Senha estão incorretos'
+        )
+    
+    return {
+        "access_token": create_access_token(usuario.user_id),
+        "refresh_token": create_refresh_token(usuario.user_id)
+    }
