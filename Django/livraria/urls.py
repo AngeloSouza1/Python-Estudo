8d8cfcf03e9175a28edb6995075c8bfa664b7782
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-12-28 19:58:34 -0300

    feat: formulario de cadastro

--- Django/livraria/urls.py
+++ Django/livraria/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.http import HttpResponse
 from livraria.views import home, logout_user, register_user
 
+
 urlpatterns = [
     path('', home, name='home'),
     path('logout/', logout_user, name='logout'),
